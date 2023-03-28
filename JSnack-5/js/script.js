c94eec37f87629a1660f3c3607390316364adae4
MarcoDeMarie <De.Marie.Marco50@gmail.com>
2023-03-28 16:33:28 +0200

    JSnack 5

--- JSnack-5/js/script.js
+++ JSnack-5/js/script.js
@@ -1,4 +1,13 @@
+const numeriDispari = []
 
+message = ''
 
+for( let i = 0; i <6; i++){
+  const numero = parseInt(prompt('inserisci un numero'))
+  if(numero %2 !==0 ){
+    numeriDispari.push(numero)
+  }
+}
 
+console.log(numeriDispari)
 
